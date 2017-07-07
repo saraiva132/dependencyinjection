ae0552ee9a7ddc350c2a460da08e782f6cdcd854
rfigueiredo <rafael.saraiva132@gmail.com>
2017-07-07 13:53:27 +0200

    Addeda second approach using the Reader Monad

--- src/main/scala/com/test/di/monad/approach2/App.scala
+++ src/main/scala/com/test/di/monad/approach2/App.scala
@@ -1,36 +1,18 @@
 package com.test.di.monad.approach2
 
-import cats.data.Reader
-
 /**
   * Solved approach1 nested dependencies problem by:
   *   - encoding the classes as functions which makes the code play nicer with Reader
   *   - composing the functions with Reader in a for comprehension and using it
   **/
-object Env {
-
-  private val persistence = new ActualPersistence
-  private val service = new ActualService
-
-  def getPersistence = Repository.mkPersistence(persistence)
-  def getService = Repository.mkService(service)
-
-}
 object Server {
 
-  import Env._
-
-  def run( program : Reader[Service, Reader[Persistence, MyHttpResponse]]) : MyHttpResponse = {
-    getPersistence(getService(program))
-  }
+  val config = new Config(new Persistence {})
 
   def start(): Unit = {
-    //Initialize Route
-    val route = new Route
-    //Lets generate a program
-    val program = route.routes("put")
+    val program = Route.routes("put", "1")
 
-    val result = run(program)
+    val result = program(config)
 
     println(result)
   }
--- src/main/scala/com/test/di/monad/approach2/Structures.scala
+++ src/main/scala/com/test/di/monad/approach2/Structures.scala
@@ -5,52 +5,39 @@ import cats.data.Reader
 case class MyHttpResponse(result: String, value: Option[Int] = None)
 
 trait Persistence {
-  def get(s: String): Int
+  def get(s: String): Int = 5
 
-  def put(a: Int): Unit
+  def put(a: Int): Unit = println(s"Inserting $a")
 
-  def delete(s: String): Unit
+  def delete(s: String): Unit = println(s"Deleting $s")
 }
 
-trait Service {
-  def getFromDB(s: String): Reader[Persistence, Int]
+case class Config(persistence: Persistence)
 
-  def putInDB(s: Int): Reader[Persistence, Unit]
-
-  def deleteFromDB(s: String): Reader[Persistence, Unit]
-}
-
-
-class ActualPersistence extends Persistence {
-  override def get(s: String): Int = 5
-
-  override def put(a: Int): Unit = println(s"Inserting $a")
-
-  override def delete(s: String): Unit = println(s"Deleting $s")
-}
-
-class ActualService extends Service {
+object Service {
 
   val p = Reader[Persistence, Persistence](identity)
 
-  type P[A] = Reader[Persistence, A]
+  def getFromDB(s: String): Reader[Persistence, Int] = p map (_.get(s))
 
-  override def getFromDB(s: String): Reader[Persistence, Int] = p map (_.get(s))
+  def putInDB(s: Int): Reader[Persistence, Unit] = p map (_.put(s))
 
-  override def putInDB(s: Int): Reader[Persistence, Unit] = p map (_.put(s))
-
-  override def deleteFromDB(s: String): Reader[Persistence, Unit] = p map (_.delete(s))
+  def deleteFromDB(s: String): Reader[Persistence, Unit] = p map (_.delete(s))
 }
 
-class Route {
-  val s = Reader[Service, Service](identity)
+object Route {
 
-  def routes(path: String, param: String = "50"): Reader[Service, Reader[Persistence, MyHttpResponse]] = path match {
-    case "get" =>
-      s map (_.getFromDB(param) map (value => MyHttpResponse("OK", Some(value))))
-    case "put" =>
-      s map (_.putInDB(param.toInt) map (_ => MyHttpResponse("OK")))
-    case "delete" =>
-      s map (_.deleteFromDB(param) map (_ => MyHttpResponse("OK")))
+  def routes(path: String, param: String = "50"): Reader[Config, MyHttpResponse] = {
+    path match {
+      case "get" => for {
+        fromDb <- Service.getFromDB(param).local[Config](_.persistence)
+      } yield MyHttpResponse("OK", Some(fromDb))
+      case "put" => for {
+        _ <- Service.putInDB(param.toInt).local[Config](_.persistence)
+      } yield MyHttpResponse("OK", None)
+      case "delete" => for {
+        _ <- Service.deleteFromDB(param).local[Config](_.persistence)
+      } yield MyHttpResponse("OK", None)
+    }
   }
 }
